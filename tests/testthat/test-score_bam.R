20212d382e8be03b0ba4c59218f8e9eeee587cc3
Rick Tankard <trickytank@users.noreply.github.com>
2022-09-16 14:09:21 +1000

    some tests for score_bam()

--- tests/testthat/test-score_bam.R
+++ tests/testthat/test-score_bam.R
@@ -1,3 +1,8 @@
+test_that("motif_cycle()", {
+  expect_equal(motif_cycles("GATA"), c("GATA", "ATAG", "TAGA", "AGAT"))
+})
+
+
 test_that("score_overlap_method()", {
   expect_equal(score_overlap_method("TTCAGGCATT", "CAG"), 6)
   expect_equal(score_overlap_method("TTCAGCAGTT", "CAG"), 6)
@@ -10,4 +15,22 @@ test_that("score_count_method()", {
   expect_equal(score_count_method("TTCAGCAGTT", "CAG"), 4)
   expect_equal(score_count_method("TTCAAGCATT", "CAG"), 2)
   expect_equal(score_count_method("CAGCAGCAGCA", "CAG"), 9)
+})
+
+test_that("set_sample_names_score_bam", {
+  # Don't know of data set to test RG extraction
+  expect_equal(
+    set_sample_names_score_bam(NULL, NGScopyData::tps_27.chr6()$bamFpath, 
+                               "basename", "\\.chr6\\.sort", ".+"), 
+    "tps_27")
+})
+
+test_that("score_bam()", {
+  expect_s3_class(read_exstra_db(system.file("extdata", "ex1_dummy_repeat.txt", package = "exSTRa")), "exstra_db")
+  expect_snapshot(
+    score_bam(
+      system.file("extdata", "ex1.bam", package = "Rsamtools"), 
+      system.file("extdata", "ex1_dummy_repeat.txt", package = "exSTRa"), 
+      sample_name_origin = "basename", groups.regex = c("case" = ""))
+  )
 })
