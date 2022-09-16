0baa441fbada28170b184de41b7cc417b91ea7de
Rick Tankard <trickytank@users.noreply.github.com>
2022-09-17 00:06:10 +1000

    read_exstra_db family tests

--- tests/testthat/test-read_exstra_db_family.R
+++ tests/testthat/test-read_exstra_db_family.R
@@ -1,16 +1,21 @@
 test_that("Read UCSC simple repeats file", {
-  expect_s3_class(
+  expect_snapshot(
     read_exstra_db(
-      system.file("extdata", "ex1_dummy_repeat.txt", package = "exSTRa")), 
-    "exstra_db"
+      system.file("extdata", "ex1_dummy_repeat.txt", package = "exSTRa")) 
   )
+  
 })
 
 test_that("Read known repeats file", {
-  expect_s3_class(
+  expect_snapshot(
     read_exstra_db(
       system.file("extdata", "repeat_expansion_disorders_grch38.txt", 
-                  package = "exSTRa")),
-    "exstra_db"
+                  package = "exSTRa"))
   )
+})
+
+test_that("read_exstra_db error catching", {
+  expect_error(read_exstra_db(5), "file must be character")
+  expect_error(read_exstra_db_ucsc(7), "file must be character")
+  expect_error(read_exstra_db_known(TRUE), "file must be character")
 })
