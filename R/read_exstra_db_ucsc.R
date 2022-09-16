fb88c17058cb4265372b697f68e7534b52257a53
Rick Tankard <trickytank@users.noreply.github.com>
2022-09-16 14:08:48 +1000

    read UCSC better

--- R/read_exstra_db_ucsc.R
+++ R/read_exstra_db_ucsc.R
@@ -8,7 +8,7 @@
 read_exstra_db_ucsc <- function(file, header, ...) {
   if (!is.character(file)) stop("file must be character")
   header.names <- c("X.bin", "chrom", "chromStart", "chromEnd", "name", "period", "copyNum", "consensusSize", "perMatch", "perIndel", "score", "A", "C", "G", "T", "entropy", "sequence")
-  col.classes <- c("integer", "factor", "integer", "integer", "factor", 
+  col.classes <- c("integer", "character", "integer", "integer", "factor", 
     "integer", "numeric", "integer", "integer", "integer", "integer", 
     "integer", "integer", "integer", "integer", "numeric", "character"
   )
@@ -35,6 +35,7 @@ read_exstra_db_ucsc <- function(file, header, ...) {
   assert("The column classes of the UCSC file are not as expected. Are you sure you are supplying a UCSCS file and that the header setting is correct?", prod(sapply(data, class) == col.classes) == 1)
   data$locus <- with(data, paste0(chrom, ":", chromStart + 1, "-", chromEnd, ":", sequence))
   data$motif <- data$sequence
+  data$strand <- "+"
   data <- data.table(data)
   data <- data[nchar(sequence) >= 2 & nchar(sequence) <= 6]
   exstra_db_new_(data, "ucsc")
