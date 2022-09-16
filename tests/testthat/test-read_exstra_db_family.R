ee94e4322b5f9ef342c16710b7fa1bcf424608cc
Rick Tankard <trickytank@users.noreply.github.com>
2022-09-17 00:25:48 +1000

    test for dodgy input

--- tests/testthat/test-read_exstra_db_family.R
+++ tests/testthat/test-read_exstra_db_family.R
@@ -14,8 +14,12 @@ test_that("Read known repeats file", {
   )
 })
 
+tf <- tempfile("wrong", fileext = "txt")
+write.table(data.frame(dog = 5, cw = 9), tf, quote = FALSE, sep = "\t", row.names = FALSE, col.names = FALSE)
+
 test_that("read_exstra_db error catching", {
   expect_error(read_exstra_db(5), "file must be character")
   expect_error(read_exstra_db_ucsc(7), "file must be character")
   expect_error(read_exstra_db_known(TRUE), "file must be character")
+  expect_error(purrr::quietly(read_exstra_db)(tf), "17 is not TRUE")
 })
