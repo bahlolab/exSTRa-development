13ff35223c5fef2ebad3ac77579e447c9ee291f4
Rick Tankard <trickytank@users.noreply.github.com>
2022-09-20 17:05:23 +1000

    Snapshot plot_names

--- tests/testthat/test-GENERICS.R
+++ tests/testthat/test-GENERICS.R
@@ -2,8 +2,4 @@ data(exstra_wgs_pcr_2)
 exstra_wgs_pcr_2_test <- exstra_wgs_pcr_2
 test_that("Plotnames works", {
   expect_snapshot(plot_names(exstra_wgs_pcr_2))
-  expect_snapshot( {
-    plot_names(exstra_wgs_pcr_2_test) <- LETTERS[1:18]; 
-    plot_names(exstra_wgs_pcr_2_test)
-  })
 })
