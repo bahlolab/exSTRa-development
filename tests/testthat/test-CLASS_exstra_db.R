3a4930dbcf9daf5d36cc70518e60ca516038982e
Rick Tankard <trickytank@users.noreply.github.com>
2022-09-20 23:20:09 +1000

    exstra_db tests

--- tests/testthat/test-CLASS_exstra_db.R
+++ tests/testthat/test-CLASS_exstra_db.R
@@ -24,15 +24,25 @@ test_that("loci on exstra_db works", {
 
 
 
-# loci_text_info.exstra_d
+# loci_text_info.exstra_db
+db_ucsc <- read_exstra_db(
+  system.file("extdata", "ex1_dummy_repeat.txt", package = "exSTRa"))
 
-# loci_normal
+test_that("loci_text_info", {
+  expect_equal(loci_text_info(db_ucsc, "seq1:21-60:AG"), "seq1:21-60:AG")
+})
 
-# loci_min_exp
+test_that("loci size", {
+  expect_equal(loci_normal(exstra_known, "HD"), 63)
+  expect_equal(loci_min_exp(exstra_known, "SCA1"), 117)
+  expect_equal(loci_normal_exp(exstra_known, "FRDA"), c(20, 600))
+  expect_equal(loci_normal_exp(db_ucsc, "seq1:21-60:AG"), c(40, NA))
+})
 
 # `[.exstra_db`
-
-# copy.exstra_db
+test_that("subsetting", {
+  expect_equal(exstra_known["HD"]$db[, long_name], "Huntington disease")
+})
 
 # verify.exstra_db()
 X_verify <- copy(exstra_known)
