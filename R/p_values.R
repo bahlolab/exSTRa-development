666ad9ccc79962ffef953f9e040e5fd296209ba2
Rick Tankard <trickytank@users.noreply.github.com>
2022-09-20 15:48:04 +1000

    Update on p_values(), removing unused code

--- R/p_values.R
+++ R/p_values.R
@@ -11,7 +11,6 @@
 #' @param alpha Significance level alpha.
 #' @param only.signif If TRUE, only return significant results.
 #' @param modify If TRUE, will modify the tsum$stats table. Effectively ignored if only.sig == TRUE.
-#' @param p.matrix Matrix of p-values for internal use. Should only be used without tsum. 
 #' @return A \code{data.table} keyed by "locus" then "sample". 
 #'         Other columns are \code{tsum} as calculated by \code{\link{tsum_test}}, \code{p.value} (uncorrected),
 #'         \code{signif} (TRUE if significant after given correction), and 
@@ -26,8 +25,7 @@ p_values <- function(
   correction = c("bf", "loci", "samples", "uncorrected"),
   alpha = 0.05,
   only.signif = FALSE,
-  modify = FALSE, 
-  p.matrix = NULL
+  modify = FALSE 
   ) {
   # verify input
   if(!missing(tsum)) {
@@ -39,24 +37,13 @@ p_values <- function(
   assert("alpha should be a probability value.", alpha >= 0, alpha <= 1)
   assert("only.signif should be a logical.", is.logical(only.signif))
   assert("", xor(is.null(p.matrix), missing(tsum)))
+  correction <- match.arg(correction)
   
   # Get p.matrix
-  if(is.null(p.matrix)) {
-    n_tests <- tsum$n_tests
-    out.table <- tsum$stats
-    if(!modify) { # make a copy so we do not modify it
-      out.table %<>% copy()
-    }
-  } else {
-    assert("p.matrix is not a matrix", is.matrix(p.matrix))
-    n_tests <- sum (!is.na(p.matrix))
-    out.table <- p.matrix %>% 
-      # NOTE: this used to be reshape2:::melt.matrix. It was changed due save a R CMD check NOTE. 
-      reshape2::melt(value.name = "p.value", 
-                             varnames = c("sample", "locus"), 
-                             as.is = TRUE
-        ) %>%
-      data.table()
+  n_tests <- tsum$n_tests
+  out.table <- tsum$stats
+  if(!modify) { # make a copy so we do not modify it
+    out.table %<>% copy()
   }
   
   # output
@@ -74,8 +61,6 @@ p_values <- function(
     out.table[!is.na(p.value), N := .N, by = sample]
     out.table[, signif := p.value <= alpha / N ]
     out.table[, N := NULL]
-  } else {
-    stop("Unknown correction method ", correction[1])
   }
   if(only.signif) {
     # Only keep significant results
