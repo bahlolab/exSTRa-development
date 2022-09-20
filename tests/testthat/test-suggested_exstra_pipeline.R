8015e5b1ed1f50be28cd70537fff973067dce2d4
Rick Tankard <trickytank@users.noreply.github.com>
2022-09-20 23:36:39 +1000

    test suggested pipeline diagram

--- tests/testthat/test-suggested_exstra_pipeline.R
+++ tests/testthat/test-suggested_exstra_pipeline.R
@@ -1,3 +1,5 @@
+suggested_file <- tempfile(fileext = ".svg")
 test_that("suggested_exstra_pipeline() check", {
   expect_null(suggested_exstra_pipeline())
+  expect_null(suggested_exstra_pipeline(suggested_file, show.with.file = TRUE))
 })
