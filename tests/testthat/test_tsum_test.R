64450b5f63bf81cb6a75b9e57988f3e083c03d69
Rick Tankard <trickytank@users.noreply.github.com>
2022-09-20 22:52:19 +1000

    tsum_test coverage

--- tests/testthat/test_tsum_test.R
+++ tests/testthat/test_tsum_test.R
@@ -2,7 +2,14 @@ data(exstra_wgs_pcr_2)
 test_that("tsum_test()", {
   expect_equal(dim(Tsum_stats$stats)[1], 36L)
   expect_equal(length(unique(Tsum_stats$stats$locus)), 2L)
-  expect_snapshot(print(tsum_test(exstra_wgs_pcr_2[c("HD", "SCA6", "SCA1", "FRDA")])))
+  expect_snapshot(
+    print(tsum_test(exstra_wgs_pcr_2[c("HD", "SCA6", "SCA1", "FRDA")])))
+  expect_s3_class(tsum_test(exstra_wgs_pcr_2[c("SCA1")], parallel = TRUE,
+                            cluster_n = 1), 
+                  "exstra_tsum")
+  expect_s3_class(tsum_test(exstra_wgs_pcr_2[c("SCA1")],
+                            case_control = TRUE), 
+                  "exstra_tsum")
 })
 
 test_that("tsum_plots", {
