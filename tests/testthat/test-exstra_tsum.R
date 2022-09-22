9d6ef74d569b4c7f964bb3b46619e952e3c02d0a
Rick Tankard <trickytank@users.noreply.github.com>
2022-09-22 12:01:26 +1000

    plot tests

--- tests/testthat/test-exstra_tsum.R
+++ tests/testthat/test-exstra_tsum.R
@@ -1,4 +1,22 @@
 test_that("exstra_tsum plots", {
   expect_null(plot(Tsum_stats))
   expect_null(plot(tsum_4))
+  vdiffr::expect_doppelganger("Tsum_stats plot", 
+                              fig = function() plot(Tsum_stats["SCA6"]))
+  vdiffr::expect_doppelganger(
+    "tsum_4 plots with sample correction", 
+    fig = function() 
+      plot(tsum_4["FRDA"], 
+           sample_col = c(WGSrpt_19 = "red", WGSrpt_09 = "green", 
+                          WGSrpt_11 = "blue"), 
+           correction = "sample")
+  )
+  vdiffr::expect_doppelganger(
+    "tsum_4 plots with 0.1 alpha", 
+    fig = function() 
+      plot(tsum_4["FRDA"], 
+        sample_col = c(WGSrpt_19 = "blue", WGSrpt_09 = "red", 
+                       WGSrpt_11 = "blue"), 
+           alpha = 0.1)
+  )
 })
