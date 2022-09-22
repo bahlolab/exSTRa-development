f8b0d836f3f3c923eefa23a71e1ef12a022e04b3
Rick Tankard <trickytank@users.noreply.github.com>
2022-09-22 13:51:17 +1000

    private function tests

---
+++ tests/testthat/test-private_functions.R
@@ -0,0 +1,16 @@
+test_that("make_quantiles_matrix()", {
+  expect_equal(2 * 2, 4)
+  expect_warning(
+    make_quantiles_matrix(exstra_wgs_pcr_2[c("HD", "SCA1")], 
+                          method = "quantile7"),
+    "More than one locus has been passed to make_quantiles_matrix"
+  )
+  expect_error(
+    make_quantiles_matrix(exstra_wgs_pcr_2[c("HD")]),
+    "Please choose type for quantile with method"
+  )
+  expect_error(
+    make_quantiles_matrix(exstra_wgs_pcr_2[c("HD")], method = "sdsa"),
+    "Undefined method sdsa"
+  )
+})
